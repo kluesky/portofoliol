a5a49abb3514b74743ffa9f4c01c80bbc6bc0910
Lyora <140289431+kluesky@users.noreply.github.com>
2025-10-19 20:21:50 +0700

    Update video-loader.js

--- video-loader.js
+++ video-loader.js
@@ -26,7 +26,7 @@ class VideoPreviewLoader {
                         size: null,
                         icon: "fas fa-file-code",
                         downloadUrl: "https://pastebin.com/qM5uJBqx",
-                        youtubeId: "Y-JpyHarAHc" // Ganti dengan ID YouTube Anda
+                        youtubeId: "uVbfSNcHshQ" // Ganti dengan ID YouTube Anda
                     }
                 ]
             },
